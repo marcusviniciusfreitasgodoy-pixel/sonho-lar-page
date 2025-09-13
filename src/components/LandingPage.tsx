7b27e2b813ea9405971a54f894e879357d47133e
marcusviniciusfreitasgodoy-pixel <marcusviniciusfreitasgodoy@gmail.com>
2025-09-13 12:53:54 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -516,7 +516,7 @@ const LandingPage = () => {
               </h3>
               
               <p className="text-lg text-luxury-navy mb-8 leading-relaxed text-left">
-                Sua decisão é respaldada por uma garantia robusta, formalizada em contrato assinado. Todos os nossos compromissos de ética, fidelidade e defesa intransigente de seus interesses estão assegurados neste documento. Assumimos o risco, pois nossa expertise é sua maior segurança, transformando a complexidade em uma conquista prazerosa e estratégica.
+                Sua decisão é respaldada por uma Garantia robusta, formalizada em contrato assinado. Todos os nossos compromissos de ética, fidelidade e defesa intransigente de seus interesses estão assegurados neste documento.
               </p>
               
               <h3 className="text-2xl font-bold mb-4 text-luxury-navy text-left">
