38e617a1d24cf506e112b28b1a17e9eac607c595
marcusviniciusfreitasgodoy-pixel <marcusviniciusfreitasgodoy@gmail.com>
2025-09-16 12:00:46 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -167,8 +167,7 @@ const LandingPage = () => {
             <h1 className="text-5xl md:text-7xl font-bold mb-6 leading-tight">
                 Descubra O Segredo dos Compradores Inteligentes
               <span className="text-luxury-gold text-4xl md:text-5xl">
-                Compre o Imóvel Que Você Quer e
-              </span>
+                Compre o Imóvel Que Você Quer e 
               <span className="text-white text-4xl md:text-5xl">
                 Pelo Preço Justo</span>
             </h1>
