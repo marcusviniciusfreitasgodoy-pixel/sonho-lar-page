546fc555156c8e2d858f3ca3b35ceb869ed5d263
marcusviniciusfreitasgodoy-pixel <marcusviniciusfreitasgodoy@gmail.com>
2025-09-16 12:25:29 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -221,12 +221,12 @@ const LandingPage = () => {
               
               <div className="space-y-4 md:space-y-6 mb-8">
                 <p className="text-lg md:text-xl text-foreground leading-relaxed">
-                  A Imobiliária que te mostra o imóvel não trabalha para você. Ela foi contratada 
-                  para. intermediar o negócio e vender pelo preço mais caro possível, pois assim ela e o Proprietário ganham mais.
+                  A Imobiliária que te mostra o imóvel não trabalha para você. Ela foi contratada pelo 
+                  Vendedor para fazer a intermediação do negócio e vender pelo preço mais caro possível.
                 </p>
                 
-                <p>
-                  <strong className="text-luxury-gold font-bold">Simples Assim!</strong>
+                <p className="text-lg md:text-xl text-foreground leading-relaxed">
+                  Elas ganham mais dinheiro quando você paga mais caro. <strong className="text-luxury-gold font-bold">Simples Assim!</strong>
                 </p>
                 
                 <p className="text-lg md:text-xl text-foreground leading-relaxed">
