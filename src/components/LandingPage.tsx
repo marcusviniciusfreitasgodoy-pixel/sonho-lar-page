e620eec549258b7c9af0981caab090a0527f7c53
marcusviniciusfreitasgodoy-pixel <marcusviniciusfreitasgodoy@gmail.com>
2025-09-16 18:03:36 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -234,7 +234,7 @@ const LandingPage = () => {
                 Ela foi contratada pelo Vendedor para fazer a intermediação do negócio e <strong>vender pelo preço mais caro possível.
                   
                 </strong> Elas ganham mais dinheiro quando você paga mais caro.<strong> Simples Assim!</strong>     
-                É como você contratar o Advogado da outra parte para te defender e acreditar que ele vai ganhar a causa pra você. Será?
+                
               </p> 
               
               <div className="bg-luxury-cream border border-luxury-gold/20 rounded-xl p-6 mb-8">
