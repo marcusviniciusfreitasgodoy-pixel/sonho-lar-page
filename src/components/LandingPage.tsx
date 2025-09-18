10f16d493129d3d38c7bcb9f7cb66ac3712b5202
marcusviniciusfreitasgodoy-pixel <marcusviniciusfreitasgodoy@gmail.com>
2025-09-18 13:53:58 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1137,7 +1137,7 @@ Você terá o controle total, transformando sua compra em uma estratégia blinda
             </div>
             
             <p className="text-sm text-white/70">
-              Apenas 6 vagas disponíveis por trimestre. Não perca sua oportunidade.
+              Para o Método Bússola Dourada disponibilizamos apenas 6 vagas disponíveis por trimestre.
             </p>
           </div>
         </div>
