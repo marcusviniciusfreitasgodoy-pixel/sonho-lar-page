5e5f3016492e40773ec1bbd156fa51d38aa1f845
marcusviniciusfreitasgodoy-pixel <marcusviniciusfreitasgodoy@gmail.com>
2025-09-18 13:47:54 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1134,10 +1134,6 @@ Você terá o controle total, transformando sua compra em uma estratégia blinda
             </Button>
             
             <div className="flex flex-col sm:flex-row gap-4 justify-center mb-8">
-              <Button variant="luxury-outline" size="xl">
-                <Mail className="mr-2 h-5 w-5" />
-                marcus@godoyprime.com.br
-              </Button>
             </div>
             
             <p className="text-sm text-white/70">
